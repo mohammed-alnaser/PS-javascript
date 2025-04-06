a509f87708bd4e7a7fdc05d5b13c23d9cdcc2c61
mohammed-alnaser <mohammedsameralnaser22@gmail.com>
2025-04-06 22:59:05 +0300

    problem 71

---
+++ problem71.js
@@ -0,0 +1,23 @@
+/*
+Trapping Rain Water – Compute how much water can be trapped between bars.
+
+*/
+
+function trap(height) {
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let water = 0;
+    
+    while (left < right) {
+        if (height[left] < height[right]) {
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+            left++;
+        } else {
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
+            right--;
+        }
+    }
+    return water;
+}
