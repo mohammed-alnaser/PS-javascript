1b058cfb3fdf3e7686f222bf37160bc2d57527c5
mohammed-alnaser <mohammedsameralnaser22@gmail.com>
2025-04-11 15:24:09 +0300

    problem 83

---
+++ problem83.js
@@ -0,0 +1,19 @@
+/**Find the First Node of a Cycle – Detect and return the starting node of a cycle in a linked list.
+ */
+
+function detectCycle(head) {
+    let slow = head, fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            slow = head;
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+}
